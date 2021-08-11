3527c428cc6e6e145e6ef6340d7a81b9c5850bc9
Dzmitry Kandrusik <dkandrusik@gmail.com>
2021-08-11 18:44:41 +0300

    version 2.0

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@test",
+        tags = "@BuyThings",
 //        dryRun = true,
         snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class Runner {
